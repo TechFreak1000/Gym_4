60a1bf51196f956882e07971866618f79ccdab5c
Rudrang Darade <rudrang04@gmail.com>
2024-04-01 12:39:57 +0530

    added customer details

---
+++ Controllers/CustomerDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Controllers
+{
+    public class CustomerDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
